fb8df243d51c8b6d0a6b63c1031fa32294513a43
Marcus Chua <chuamarcusanselm@gmail.com>
2025-11-08 15:34:07 +0900

    fast-api-basics, Project 1 done

--- Project 1/books.py
+++ Project 1/books.py
@@ -18,59 +18,63 @@ async def read_all_books():
     return BOOKS
 
 
+
 @app.get("/books/{book_title}")
 async def read_book(book_title: str):
     for book in BOOKS:
-        if book.get('title').casefold() == book_title.casefold():
+        if book.get('title').casefold() == book_title.casefold(): #casefold = toLower
             return book
 
-
-@app.get("/books/")
-async def read_category_by_query(category: str):
+@app.get("/books/") #anything that passed / that isn't a path parameter == query_param
+async def read_catergory_by_query(category: str):
     books_to_return = []
+     
     for book in BOOKS:
         if book.get('category').casefold() == category.casefold():
             books_to_return.append(book)
     return books_to_return
 
-
-# Get all books from a specific author using path or query parameters
-@app.get("/books/byauthor/")
+app.get('/books/by_author/{author}')
 async def read_books_by_author_path(author: str):
     books_to_return = []
     for book in BOOKS:
         if book.get('author').casefold() == author.casefold():
             books_to_return.append(book)
-
     return books_to_return
-
-
-@app.get("/books/{book_author}/")
+     
+@app.get('/books/{book_author}/')
 async def read_author_category_by_query(book_author: str, category: str):
     books_to_return = []
     for book in BOOKS:
         if book.get('author').casefold() == book_author.casefold() and \
-                book.get('category').casefold() == category.casefold():
+            book.get('category').casefold() == category.casefold():
             books_to_return.append(book)
-
     return books_to_return
 
 
-@app.post("/books/create_book")
+@app.post('/books/create_book')
 async def create_book(new_book=Body()):
     BOOKS.append(new_book)
-
+    return BOOKS
 
 @app.put("/books/update_book")
 async def update_book(updated_book=Body()):
     for i in range(len(BOOKS)):
         if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
             BOOKS[i] = updated_book
-
+    return
 
 @app.delete("/books/delete_book/{book_title}")
 async def delete_book(book_title: str):
     for i in range(len(BOOKS)):
-        if BOOKS[i].get('title').casefold() == book_title.casefold():
+        if BOOKS[i].get('title').casefold()== book_title.casefold():
             BOOKS.pop(i)
             break
+    return
+        
+
+
+# # dynamic_params matches the entire path after /books/
+# @app.get("/books/{dynamic_params}")
+# async def read_all_books (dynamic_params):
+#     return {dynamic_params: dynamic_params}
